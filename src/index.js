3928ad50b70d8154841330ff0f3ff5fff6280197
Laura <laurakbrandon@gmail.com>
2020-05-04 20:26:17 +1000

    blink an led to test raspberry pi gpio

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(17, 'out');
+const button = new Gpio(4, 'in', 'rising', { debounceTimeout: 10 });
+
+button.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+
+  led.writeSync(led.readSync() ^ 1);
+});
+
+process.on('SIGINT', _ => {
+  led.unexport();
+  button.unexport();
+});
