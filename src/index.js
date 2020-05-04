819ca3667ab66cedd2ca96010783cfb587bcbd33
Laura <laurakbrandon@gmail.com>
2020-05-04 20:28:04 +1000

    add linting and pre push hook

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'rising', { debounceTimeout: 10 });
 
+/* eslint-disable-next-line no-unused-vars */
 button.watch((err, value) => {
   if (err) {
     throw err;
@@ -10,7 +11,7 @@ button.watch((err, value) => {
   led.writeSync(led.readSync() ^ 1);
 });
 
-process.on('SIGINT', _ => {
+process.on('SIGINT', () => {
   led.unexport();
   button.unexport();
 });
